4343d7fbe0a53df489190791a053c3c8947775c9
ardanvk <93476549+ardanvk@users.noreply.github.com>
2021-11-03 20:34:37 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,37 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
