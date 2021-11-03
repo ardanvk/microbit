bf353d6de9b658b0c1160013f18ca8f96aaf8595
ardanvk <93476549+ardanvk@users.noreply.github.com>
2021-11-03 21:20:27 +0100

    Herstel '3-11-2021 20:37:25 0.0.1'

--- main.ts
+++ main.ts
@@ -1,10 +1,37 @@
-basic.forever(function () {
-    basic.showNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 25) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 99)
-    } else if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
-        maqueen.servoRun(maqueen.Servos.S2, 90)
-    } else {
-        maqueen.motorStop(maqueen.Motors.All)
-    }
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
 })
