3937bdefe355b275a028b109a1bf69519d2a5944
ardanvk <93476549+ardanvk@users.noreply.github.com>
2021-11-03 21:18:55 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,37 +1,10 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        # # . # #
-        # # . # #
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-})
-input.onGesture(Gesture.Shake, function () {
-    basic.clearScreen()
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showLeds(`
-        # # . # #
-        # # . # #
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        # # . # #
-        # # . # #
-        . . . . .
-        . # # # .
-        # . . . #
-        `)
+basic.forever(function () {
+    basic.showNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 25) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 99)
+    } else if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        maqueen.servoRun(maqueen.Servos.S2, 90)
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
